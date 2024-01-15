f4380d3908c939647488d2065263c185acefd138
wesley berto <wesleyberto4@gmail.com>
2024-01-15 19:02:59 -0300

    primeiros passos

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import './Home.css'
+
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Home</h1 >
+            <img src="https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg" alt="" />
+        </>
+    )
+}
+
+export default Home;
