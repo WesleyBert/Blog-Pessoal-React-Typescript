04214a685373c7c03527f4c3e2fd707c526cb45d
wesley berto <wesleyberto4@gmail.com>
2024-01-18 07:47:57 -0300

    Adicionando routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,24 @@
 import Home from './pages/Home'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './components/Login/Login'
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,23 +1,14 @@
-import { Link, useNavigate } from "react-router-dom";
-
-
 
 function Login() {
-
-    let navigate = useNavigate()
     return (
-        <div>
-            <h2 className="text-slate-900 text-5x1">Login</h2>
-            <div>
-                <button type='submit'
-                    onClick={() => { navigate('/home') }}
-                    className="hover:underline mx-4"
-                >Login useNavigate
-                </button>
-                <Link to="/Home" className="hover:underline mx-4">Login por Link</Link>
+        <>
+            <div className="h-96 mb-96">
+
+                <h1 className='text-xl'>Login</h1>
+
             </div>
-        </div>
-    )
+        </>
+    );
 }
 
 export default Login;
