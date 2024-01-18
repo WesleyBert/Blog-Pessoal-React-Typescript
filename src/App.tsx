d26b49a85402208946d9ec6e943657406223c125
wesley berto <wesleyberto4@gmail.com>
2024-01-18 17:26:02 -0300

    login e cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./components/Login/Login"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Navbar from "./components/Navbar/Navbar"
+import { AuthProvider } from "./contexts/AuthContext"
+import Footer from "./components/Footer/Footer"
 
-import Home from './pages/Home'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Login from './components/Login/Login'
 
 function App() {
 
+
+
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className='min-h-[80vh]'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/cadastro" element={<Cadastro />}></Route>
+              <Route path="/home" element={<Home />}></Route>
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
