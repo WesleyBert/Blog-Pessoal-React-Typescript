7b334eab25bf1cb5c9d371be071e719dd890d34b
wesley berto <wesleyberto4@gmail.com>
2024-01-31 09:31:14 -0300

    adicionando configuracoes para o deploy

--- src/components/postagens/postagens/CardPostagem.tsx
+++ src/components/postagens/postagens/CardPostagem.tsx
@@ -9,7 +9,7 @@ function CardPostagem({ post }: CardPostagensProps) {
     return (
         <div className="border-slate-900 border flex flex-col rounded overflow-hidden justify-between">
             <div>
-                <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
+                <div className="flex w-full bg-indigo-600 py-2 px-4 items-center gap-4">
                     <img src={ post.usuario?.foto } className="h-12 rounded-full" alt="" />
                     <h3 className="text-lg font-bold text-center uppercase">{ post.usuario?.nome}</h3>
                 </div>
@@ -23,7 +23,7 @@ function CardPostagem({ post }: CardPostagensProps) {
                     }).format(new Date(post.data))}</p>
                 </div>
                 <div className="flex">
-                    <Link to={`/editarPostagem/${post.id}`} className="w-full text-white bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2">
+                    <Link to={`/editarPostagem/${post.id}`} className="w-full text-white bg-indigo-600 hover:bg-indigo-800 flex items-center justify-center py-2">
                         <button>Editar</button>
                     </Link>
                     <Link to={`/deletarPostagem/${post.id}`} className="text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center">
